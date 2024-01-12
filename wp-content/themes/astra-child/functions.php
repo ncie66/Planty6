e9e12ee4dd667d63f2c15eefd1bca45db32e9e20
ncie66 <ym66300@gmail.com>
2024-01-12 13:42:06 +0100

    first commit

---
+++ wp-content/themes/astra-child/functions.php
@@ -0,0 +1,7 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'enqueue_astra_child_styles' );
+function enqueue_astra_child_styles() {
+    wp_enqueue_style( 'astra-child-style', get_stylesheet_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'astra-parent-style', get_template_directory_uri() . '/style.css' );
+}
+?>

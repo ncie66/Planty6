ad87c1bb90aa42d3a6af26cc7eaa287009971399
ncie66 <ym66300@gmail.com>
2024-01-27 01:53:56 +0100

    Rajout des fichiers et dossiers ignorés

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -1,7 +1,6 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'enqueue_astra_child_styles' );
-function enqueue_astra_child_styles() {
-    wp_enqueue_style( 'astra-child-style', get_stylesheet_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'astra-parent-style', get_template_directory_uri() . '/style.css' );
-}
+    add_action( 'wp_enqueue_scripts', 'enqueue_astra_child_styles' );
+    function enqueue_astra_child_styles() {
+        wp_enqueue_style( 'astra-child-style', get_stylesheet_directory_uri() . '/style.css' );
+    }
 ?>
